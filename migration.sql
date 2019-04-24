faab3a9e27018138ab5e53b1b38c82c6832e4de7
Bethany Ramirez <bethany.e.ramirez@gmail.com>
2019-04-24 14:06:51 -0500

    set up migration file

--- migration.sql
+++ migration.sql
@@ -1,22 +1,45 @@
-USE adlister_db;
+CREATE DATABASE IF NOT EXISTS pets_db;
+USE pets_db;
+
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS charPivot;
+DROP TABLE IF EXISTS characteristics;
 
 CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+                     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                     name VARCHAR(240) NOT NULL UNIQUE,
+                     email VARCHAR(240) NOT NULL,
+                     password VARCHAR(255) NOT NULL,
+                     PRIMARY KEY (id)
 );
 
 CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+                   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                   user_id INT UNSIGNED NOT NULL,
+                   title VARCHAR(250) NOT NULL,
+                   description VARCHAR(250) NOT NULL,
+                   createDate DATE NOT NULL,
+                   soldDate DATE,
+                   zipCode int,
+                   dob DATE,
+                   gender VARCHAR(1) NOT NULL,
+                   pictureURL VARCHAR(250),
+                   PRIMARY KEY (id),
+                   FOREIGN KEY (user_id) REFERENCES users(id)
+                     ON DELETE CASCADE
+);
+
+CREATE TABLE characteristics (
+                               id INT UNSIGNED NOT NULL,
+                               characteristic VARCHAR(240) NOT NULL,
+                               PRIMARY KEY (id)
+);
+
+CREATE TABLE charPivot (
+                         ad_id INT UNSIGNED NOT NULL,
+                         char_id INT UNSIGNED NOT NULL,
+                         PRIMARY KEY (ad_id),
+                         FOREIGN KEY (char_id) REFERENCES characteristics(id)
 );
