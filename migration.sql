60ce026999d28aaf333f76da6b81b49efcc8482c
Bethany Ramirez <bethany.e.ramirez@gmail.com>
2019-04-24 14:38:23 -0500

    set up sql connection

--- migration.sql
+++ migration.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS characteristics;
 
 CREATE TABLE users (
                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                     name VARCHAR(240) NOT NULL UNIQUE,
+                     username VARCHAR(240) NOT NULL UNIQUE,
                      email VARCHAR(240) NOT NULL,
                      password VARCHAR(255) NOT NULL,
                      PRIMARY KEY (id)
